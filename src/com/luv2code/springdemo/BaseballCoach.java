d52e1ee4bd2b79845fa78af778744f5616bf0b17
mark <marcelonipromucena@gmail.com>
2020-09-30 19:32:14 -0400

    added spring jars

---
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting pratice";
+	}
+
+	@Override
+	public String getDaiyFortune() {
+		return fortuneService.getFortune();
+	}
+
+	
+
+}
---
+++ src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Esporte doido";
+	}
+
+	@Override
+	public String getDaiyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
